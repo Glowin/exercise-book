42d52ec1c499c131fe9b71e1c903f005c6bb3da4
Glowin <jiangbian66@gmail.com>
2013-06-27 08:55:36 +0800

    change the route method

--- node/express/microblog/app.js
+++ node/express/microblog/app.js
@@ -28,7 +28,9 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
+app.get('/hello', routes.hello);
 app.get('/users', user.list);
+app.get('/user/:username', user.name);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- node/express/microblog/routes/user.js
+++ node/express/microblog/routes/user.js
@@ -6,3 +6,7 @@
 exports.list = function(req, res){
   res.send("respond with a resource");
 };
+
+exports.name = function(req, res) {
+  res.send('User name is ' + req.params.username);
+};
