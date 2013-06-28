0484d3a14c680fabd9766ae350dc7280dc60e3ea
Glowin <jiangbian66@gmail.com>
2013-06-28 13:59:46 +0800

    add db.js

---
+++ node/express/microblog/models/db.js
@@ -0,0 +1,7 @@
+var settings = require('../settings');
+var Db = require('mongodb').Db;
+var Connection = require('mongodb').Connection;
+var Server = require('mongodb');
+
+module.exports = new Db(settings.db,
+	new Server(settings.host, Connection.DEFAULT_PORT, {}));
